404316be4c8785717f30359a690d10bdd3adff89
WAR-DEPT <paranoidart5101@gmail.com>
2024-03-30 20:16:10 +0530

    croissant: Add some bordconfig files

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,8 +36,9 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00008000
 BOARD_RAMDISK_OFFSET := 0x01000000 
 BOARD_TAGS_OFFSET := 0x00000100
+BOARD_KERNEL_IMAGE_NAME := Image
 
-TARGET_KERNEL_CONFIG := croissant_defconfig 
+TARGET_KERNEL_CONFIG := ze554kl-user-sdm660-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/asus/croissant
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 
@@ -48,7 +49,8 @@ BOARD_KERNEL_CMDLINE := msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE := lpm_levels.sleep_disabled=1 
 BOARD_KERNEL_CMDLINE := sched_enable_hmp=1 
 BOARD_KERNEL_CMDLINE := sched_enable_power_aware=1 
-BOARD_KERNEL_CMDLINE := service_locator.enable=1 swiotlb=1 
+BOARD_KERNEL_CMDLINE := service_locator.enable=1 
+BOARD_KERNEL_CMDLINE := swiotlb=1 
 BOARD_KERNEL_CMDLINE := androidboot.configfs=true 
 BOARD_KERNEL_CMDLINE := androidboot.usbcontroller=a800000.dwc3 
 
